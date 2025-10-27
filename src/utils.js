a1ab1f251143d96afea5ba1194560727ab6a128f
josephine anyango <anyangojozey@gmail.com>
2025-10-27 12:20:13 +0300

    done

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
-// Your code here
+// src/utils.js
 export function pointsForWord(word) {
   let points = 0;
+
+  // Loop through each character in the word
   for (const char of word) {
-    if (["a", "e", "i", "o", "u"].includes(char)) {
-      points += 1;
-    } else {
-      points += 2;
-    }
+    // Use regex to test vowels (case-insensitive)
+    points += /[aeiou]/i.test(char) ? 1 : 2;
   }
+
   return points;
 }
